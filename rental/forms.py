a36e01ef0e99dd0558dcce361205f2e297b40a12
adji py <mochadjiprasetyo@gmail.com>
2025-07-04 10:22:19 +0700

    update juli

--- rental/forms.py
+++ rental/forms.py
@@ -10,7 +10,7 @@ class MotorForm(forms.ModelForm):
     tersedia = forms.ChoiceField(choices=STATUS_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
     class Meta:
         model = Motor
-        fields = '__all__'
+        fields = ['merk', 'plat_nomor', 'harga_sewa_per_hari', 'tersedia', 'jumlah_unit']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -22,7 +22,7 @@ class MotorForm(forms.ModelForm):
 class PelangganForm(forms.ModelForm):
     class Meta:
         model = Pelanggan
-        fields = '__all__'
+        fields = ['nama', 'jenis_kelamin', 'no_ktp', 'status', 'alamat']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -30,20 +30,47 @@ class PelangganForm(forms.ModelForm):
             field.widget.attrs.update({'class': 'form-control'})
 
 class TransaksiForm(forms.ModelForm):
+    total_harga = forms.DecimalField(
+        required=False,
+        widget=forms.NumberInput(attrs={
+            'readonly': 'readonly',
+            'class': 'form-control'
+        })
+    )
     class Meta:
         model = Transaksi
-        fields = ['motor', 'pelanggan', 'tanggal_sewa', 'tanggal_kembali', 'status_pengembalian']
+        fields = [
+            'motor',
+            'pelanggan',
+            'tanggal_sewa',
+            'tanggal_kembali',
+            'status_pengembalian'
+        ]
         widgets = {
-            'tanggal_sewa': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'tanggal_kembali': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'status_pengembalian': forms.Select(choices=[(True, 'Sudah'), (False, 'Belum')], attrs={'class': 'form-control'}),
+            'tanggal_sewa': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control'
+            }),
+            'tanggal_kembali': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control'
+            }),
+            'status_pengembalian': forms.Select(
+                choices=[(True, 'Sudah'), (False, 'Belum')],
+                attrs={'class': 'form-control'}
+            ),
         }
-
-    total_harga = forms.DecimalField(required=False, widget=forms.NumberInput(attrs={'readonly': 'readonly', 'class': 'form-control'}))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Tambahkan label kosong custom untuk dropdown FK
+        self.fields['motor'].empty_label = "Pilih Motor: "
+        self.fields['pelanggan'].empty_label = "Pilih Pelanggan: "
+
+        # Tambahkan class form-control ke semua field (kecuali status_pengembalian yang sudah)
         for name, field in self.fields.items():
-            if name != 'status_pengembalian':
+            if name not in ['status_pengembalian', 'tanggal_sewa', 'tanggal_kembali']:
                 field.widget.attrs.update({'class': 'form-control'})
+
 
